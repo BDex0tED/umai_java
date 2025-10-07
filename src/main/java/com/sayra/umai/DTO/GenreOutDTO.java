76303ad789bbbb1a5a770c10d80c819786b5f20a
Manas <bemanaff@gmail.com>
2025-10-07 22:16:06 +0600

    Genre was enhanced, now creating genres and connecting the works with genres is available

---
+++ src/main/java/com/sayra/umai/DTO/GenreOutDTO.java
@@ -0,0 +1,13 @@
+package com.sayra.umai.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenreOutDTO {
+    private Long id;
+    private String name;
+}
